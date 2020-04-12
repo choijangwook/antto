5bc193a87d594bd87b92b00cf37becdf046b8c52
minamAn <minamhunmin@gmail.com>
2020-04-12 10:27:42 +0900

    Updates

---
+++ precache-manifest.3ed7e63937cc952a9d40240d7b97bcaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ea38f292425c641fb609441718a805",
+    "url": "/antto/index.html"
+  },
+  {
+    "revision": "e0ac49a52a884b77146b",
+    "url": "/antto/static/css/main.c914ae08.chunk.css"
+  },
+  {
+    "revision": "1e47ebc173b4a84bf2e3",
+    "url": "/antto/static/js/2.28a20c18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/antto/static/js/2.28a20c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ac49a52a884b77146b",
+    "url": "/antto/static/js/main.6e903583.chunk.js"
+  },
+  {
+    "revision": "e665a26d1250f37f9b99",
+    "url": "/antto/static/js/runtime-main.713b8b4e.js"
+  }
+]);
