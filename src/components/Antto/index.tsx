0031bbe1a28ef22968462d5b5faecaaed4e0c007
Anminam <minamhunmin@gmail.com>
2020-04-13 09:38:40 +0900

    update hook

--- src/components/Antto/index.tsx
+++ src/components/Antto/index.tsx
@@ -1,29 +1,69 @@
-import React from 'react';
+import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
 import Ball from 'components/Ball';
 
 
 const Antto = () => {
-    const list = getNumber();
-    const balls = list.slice(0,6);
-    let bounsBall = list.slice(6,7)[0];
+
+    const lottoNumbers = useMemo(() => getNumbers(), []);
+    const [winNumbers, setWinNumbers] = useState<number[]>(lottoNumbers);
+    const [winBalls, setWinBalls] = useState<number[]>([]);
+    const [bouns, setBouns] = useState<number | null>(null);
+    const [redu, setRedo] = useState(false);
+    const timeouts = useRef<any[]>([]);
+    const TIME = 100;
+    useEffect(()=> {
+        console.log('useEffect');
+        for( let i = 0; i < winNumbers.length - 1; i++) {
+            timeouts.current[i] = setTimeout( () => {
+                setWinBalls((prevBalls)=> [...prevBalls, winNumbers[i]])
+            }, (i + 1 ) * TIME);
+        }
+
+        timeouts.current[6] = setTimeout( ()=> {
+            setBouns(winNumbers[6]);
+            setRedo(true);
+        }, 7 * TIME);
+        return () => {
+            timeouts.current.forEach((v) => {
+                clearTimeout(v);
+            })
+        }
+    }, [winNumbers]);
+
+    const onClickRedo = useCallback(() => {
+        console.log('onClickRedo');
+        console.log(winNumbers);
+        setWinNumbers(getNumbers());
+        setWinBalls([]);
+        setBouns(null);
+        setRedo(false)
+        timeouts.current = [];
+    },[])
+
     
     return (
         <main>
+            <h1>당첨번호</h1>
             {
-                balls.map((i) => {
-                    return <Ball number={i} />
+                winBalls.map((i) => {
+                    return <Ball key={ i } number={i} />
                 })
             }
             <div>보너스볼</div>
             {
-                <Ball number={bounsBall} />
+                bouns &&
+                <Ball number={bouns} />
             }
+            <div>
+                <button type="button" onClick={onClickRedo}>한번더</button>
+            </div>
         </main>
     )
 }
 
-const getNumber = (): number[] => {
-    console.log(`call getNumber`)
+
+const getNumbers = (): number[] => {
+    console.log(`call getNumber`);
     const arr = Array(45).fill(0).map((v, i) => i+1);
     const shuffle = [];
     while(arr.length > 0) {
